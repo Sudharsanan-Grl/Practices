621ccc7a1976d2c7e479f977cba736e6875683c1
Sudharsanan-Grl <124996741+Sudharsanan-Grl@users.noreply.github.com>
2023-03-30 18:25:18 +0530

    changed to html file output

--- sample programs/CSVFILE/CSVFILE/TestCase4211.cs
+++ sample programs/CSVFILE/CSVFILE/TestCase4211.cs
@@ -5,6 +5,9 @@ using System.Text;
 using System.Threading.Channels;
 using System.Threading.Tasks;
 using static CSVFILE.Packet;
+using System.IO;
+using System.Drawing;
+
 namespace CSVFILE
 {
     internal class TestCase4211
@@ -15,19 +18,27 @@ namespace CSVFILE
            if( CmdList.Contains(CmdType.TestStart))
             {
                 ValHPDTimeDiff(CmdList, StartTimeList);
+
                 foreach (var line in TestCasesResults)
                 {
                     Console.WriteLine(line);
+
                 }
+                
 
             }
+            using (StreamWriter writer = new StreamWriter("E:\\RawData\\sample.html"))
+            {
+                writer.WriteLine(TestCasesResults[0]);
+            }
+
         }
 
         private void ValHPDTimeDiff(List<CmdType> CmdList, List<double> StartTimeList)
         {
             int HPDAssertedIndex = CmdList.IndexOf(CmdType.HPD_Asserted);
             int HPDRemovedIndex = CmdList.IndexOf(CmdType.HPD_Removed);
-            if ((StartTimeList[HPDAssertedIndex] - StartTimeList[HPDRemovedIndex]) * 1e3 >= 200)
+            if ((StartTimeList[HPDAssertedIndex] - StartTimeList[HPDRemovedIndex]) * 1e3 >= 2)
             {
                 TestCasesResults.Add($"Step 1 ::[PASS]:HPD Asserted and HPD Removed Time difference  Exp is atleast : 2ms Obt:{(StartTimeList[HPDAssertedIndex] - StartTimeList[HPDRemovedIndex]) * 1e3}ms Start index#{HPDAssertedIndex} Stop index#{HPDRemovedIndex}");
             }
@@ -36,21 +47,17 @@ namespace CSVFILE
                 TestCasesResults.Add($"Step 1 ::[FAIL]:HPD Asserted and HPD Removed Time difference  Exp is atleast : 2ms Obt:{(StartTimeList[HPDAssertedIndex] - StartTimeList[HPDRemovedIndex]) * 1e3}ms Start index#{HPDAssertedIndex} Stop index#{HPDRemovedIndex}");
 
             }
-            // TestCasesResults[0].Contains("[PASS]");
-           
 
-            const string GREEN = "\u001b[32m";
-            const string RED = "\u001b[31m";
-            const string RESET = "\u001b[0m";
 
-            // Create a string with colored text
-            string coloredPass = GREEN + "PASS" + RESET;
-            string coloredFail = RED + "FAIL" + RESET;
+            string coloredPass = "<b style='color:green;'>PASS</b>";
+            string coloredFail = "<p style='color:red;'>FAIL</p>";
 
-            for(int i = 0; i < TestCasesResults.Count; i++)
+
+            for (int i = 0; i < TestCasesResults.Count; i++)
             {
                 TestCasesResults[i] = TestCasesResults[i].Replace("PASS", coloredPass);
                 TestCasesResults[i] = TestCasesResults[i].Replace("FAIL", coloredFail);
+                Console.WriteLine(TestCasesResults[i]);
 
             } 
            
