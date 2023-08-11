e5f54aad817afe9d1a24c793ade328c27bcd605c
Sudharsanan-Grl <124996741+Sudharsanan-Grl@users.noreply.github.com>
2023-08-11 16:56:43 +0530

    MS UNIT TESTING

---
+++ sample programs/MSUnitPractice/MSUnitPractice/MSUnitTest.cs
@@ -0,0 +1,35 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MSUnitPractice
+{
+    [TestClass]
+    public class MSUnitTest
+    {
+        [TestMethod]
+        public void MSTestMethod1()
+        {
+            int x = 10;
+            MSUnitTest mSUnitTest = new MSUnitTest();
+            int y = mSUnitTest.DoubleMethod(5);
+            Assert.AreEqual(x, y);
+        }
+        [TestMethod]
+        [DataRow(10)]
+        [DataRow(5)]
+        [DataRow(20)]
+        public void MSTestMethod2(int y)
+        {
+            int x = 20;
+            MSUnitTest mSUnitTest = new MSUnitTest();
+
+            int z = mSUnitTest.DoubleMethod(y);
+            Assert.IsTrue(z>=x);
+        }
+        public int DoubleMethod(int x)
+        {
+            x = x * 2;
+            return x;
+        }
+    }
+
+}
