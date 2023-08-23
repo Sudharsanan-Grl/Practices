a713b64037ce3bc8f569218db05d6eddc629a58e
Sudharsanan-Grl <124996741+Sudharsanan-Grl@users.noreply.github.com>
2023-08-23 18:52:51 +0530

    Parent Child

---
+++ sample programs/Inheretance2/Inheretance2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Inheritance2
+{
+    public class ClassOne
+    {
+        public ClassOne(string s) 
+        {
+            Console.WriteLine("Class 1 constructor " + s);
+        }
+    }
+    public class ClassTwo :ClassOne
+    {
+        public ClassTwo(a) : base(a) 
+        {
+            Console.WriteLine("Class 2 constructor");
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassTwo classTwo = new ClassTwo();
+        }
+    }
+}
