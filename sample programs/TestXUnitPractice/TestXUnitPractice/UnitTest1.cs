3c0ffcf0b3cc856cc279bec7194afb7989709c8d
Sudharsanan-Grl <124996741+Sudharsanan-Grl@users.noreply.github.com>
2023-09-01 15:51:55 +0530

    Unit Test

--- sample programs/TestXUnitPractice/TestXUnitPractice/UnitTest1.cs
+++ sample programs/TestXUnitPractice/TestXUnitPractice/UnitTest1.cs
@@ -2,25 +2,38 @@ using Xunit;
 
 namespace TestXUnitPractice
 {
+    //Unit testing is a software testing methodology where individual units or components of a program
+    //are tested to ensure they are functioning as expected.
+
+
     public class UnitTest1
     {
+        //this attributes allows to run the method
        [Fact]
         public void Test1()
         {
+            //if it is false then the testcase is pass
             Assert.False(false,"this is not going to fail");
 
         }
+        //this attributes allows to run the method
+
         [Fact]
         public void Test2()
         {
+            //if it is true then the testcase is pass
             Assert.True(true, "this is not going to fail");
 
         }
+        //this attributes allows to run the method
+
         [Fact]
         public void Test3()
         {
             int expected = 100;
             int actual = 10 * 10;
+
+            //if it is equal then the testcase is pass
             Assert.Equal(expected, actual);
         }
       
@@ -34,6 +47,7 @@ namespace TestXUnitPractice
 
          public class MathOperationsTests
          {
+            //this attribute allows to run the method with each of the data indivudually
              [Theory]
              [InlineData(2, 3, 5)]
              [InlineData(0, 0, 0)]
@@ -46,8 +60,9 @@ namespace TestXUnitPractice
             
               int result = math.Add(a, b);
 
-           
-              Assert.Equal(expected, result);
+                //if it is equal then the testcase is pass
+
+                Assert.Equal(expected, result);
              }
          }
 
