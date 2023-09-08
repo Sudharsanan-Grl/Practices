9841fcd69e4c03bcf1379a260cb9f66abb54781d
Sudharsanan-Grl <124996741+Sudharsanan-Grl@users.noreply.github.com>
2023-09-08 20:20:36 +0530

    Enum

---
+++ sample programs/Enum/Enum/Program.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+namespace EnumNameSpace
+{
+    //Enum CLass
+ public class EnumClass
+ {
+        //Enum Fields
+     public enum Color
+     {
+        red,
+        blue,
+        green
+     }
+        //enum field
+        public Color ColorValue;
+        //this method converts string into enum
+        public void convertion(string r)
+        {
+            //converting string to enum using parse
+            ColorValue = (Color)Enum.Parse(typeof(Color), r);
+
+            //printing the result
+            Console.WriteLine(ColorValue.ToString());
+        }
+
+      static void Main(string[] args)
+        {
+            //creating string with the enum name
+            string s = "red";
+            EnumClass obj= new EnumClass();
+            //calling the method
+            obj.convertion(s);
+            
+        }
+  }
+
+}
