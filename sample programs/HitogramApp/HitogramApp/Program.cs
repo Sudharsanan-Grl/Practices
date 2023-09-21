7e2126434109ac97527201bad6dac3d911d67223
Sudharsanan-Grl <124996741+Sudharsanan-Grl@users.noreply.github.com>
2023-09-21 18:17:50 +0530

    Histogram

--- sample programs/HitogramApp/HitogramApp/Program.cs
+++ sample programs/HitogramApp/HitogramApp/Program.cs
@@ -1,8 +1,8 @@
-﻿using CsvHelper;
-using CsvHelper.Configuration;
-using HitogramApp;
+﻿using CsvHelper.Configuration;
+using CsvHelper;
+using OxyPlot.Series;
 using System.Globalization;
-using System.Net.Sockets;
+using System.Web.Helpers;
 
 namespace HistogramApp
 {
@@ -30,8 +30,7 @@ namespace HistogramApp
             // Find the minimum and maximum voltage levels
             double minVoltage = signalData.Min();
             double maxVoltage = signalData.Max();
-            Console.WriteLine(minVoltage);
-            Console.WriteLine(maxVoltage);
+           
             // Define the number of intervals (255 in this case)
             int numIntervals = 255;
 
@@ -55,12 +54,67 @@ namespace HistogramApp
                 }
             }
 
-            // Display the histogram
-            Console.WriteLine("Histogram of Voltage Levels:");
-            for (int i = 0; i < numIntervals; i++)
+            string histogramCsvFilePath = @"E:\Cap0\HistogramData.csv";
+
+            using (var writer = new StreamWriter(histogramCsvFilePath))
+            using (var csv = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture)))
             {
-                Console.WriteLine($"{i}: {histogram[i]}");
+                // Write the histogram data to the CSV file
+                for (int i = 0; i < numIntervals; i++)
+                {
+                    csv.WriteRecord(new { Interval = i, Count = histogram[i] });
+                    Console.WriteLine( i +" "+ histogram[i] );
+                }
             }
+
+            // Display a message indicating where the CSV is saved
+            Console.WriteLine($"Histogram data saved to: {histogramCsvFilePath}");
+
+
+
+            string htmlFilePath = @"E:\Cap0\HistogramChart.html";
+            using (var writer = new StreamWriter(htmlFilePath))
+            {
+                writer.WriteLine("<html>");
+                writer.WriteLine("<head>");
+                writer.WriteLine("  <script type='text/javascript' src='https://www.gstatic.com/charts/loader.js'></script>");
+                writer.WriteLine("  <script type='text/javascript'>");
+                writer.WriteLine("    google.charts.load('current', { packages: ['corechart'] });");
+                writer.WriteLine("    google.charts.setOnLoadCallback(drawChart);");
+                writer.WriteLine("    function drawChart() {");
+                writer.WriteLine("      var data = google.visualization.arrayToDataTable([");
+
+                // Write histogram data to the HTML
+                writer.WriteLine("        ['Interval', 'Count'],");
+                for (int i = 0; i < numIntervals; i++)
+                {
+                    writer.WriteLine($"        ['{i}', {histogram[i]}],");
+                }
+                writer.WriteLine("      ]);");
+
+                writer.WriteLine("      var options = {");
+                writer.WriteLine("        title: 'Histogram of Voltage Levels',");
+                writer.WriteLine("        legend: { position: 'none' },");
+                writer.WriteLine("      };");
+
+                writer.WriteLine("      var chart = new google.visualization.Histogram(document.getElementById('chart_div'));");
+                writer.WriteLine("      chart.draw(data, options);");
+                writer.WriteLine("    }");
+                writer.WriteLine("  </script>");
+                writer.WriteLine("</head>");
+                writer.WriteLine("<body>");
+                writer.WriteLine("  <div id='chart_div' style='width: 900px; height: 500px;'></div>");
+                writer.WriteLine("</body>");
+                writer.WriteLine("</html>");
+            }
+
+            // Display a message indicating where the HTML file is saved
+            Console.WriteLine($"Histogram chart saved to: {htmlFilePath}");
+
+
+
+
+
         }
     }
     
